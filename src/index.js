7ab62daef542860fc9778267e354d3ab01411faa
Takuya Matsuyama <hi@craftz.dog>
2019-02-17 12:24:11 +0900

    feat(deps): Update stack

--- src/index.js
+++ src/index.js
@@ -1,40 +1,62 @@
 // @flow
-import toMarkdown from 'to-markdown'
+import TurndownService from 'turndown'
+import { gfm } from 'turndown-plugin-gfm'
 
-export default function HTML2Markdown (html: string): string {
-  let md = toMarkdown(html, {
-    gfm: true,
-    converters: [
-      {
-        filter: ['div', 'p', 'dt', 'dd', 'summary'],
-        replacement (innerHTML) {
-          return '\n' + innerHTML + '\n'
-        }
-      },
-      {
-        filter: ['font', 'span', 'article', 'section', 'nav', 'button', 'main', 'footer', 'header', 'aside', 'details', 'u', 'samp', 'var', 'kbd', 'legend', 'mark', 'output', 'small', 'sub', 'sup'],
-        replacement (innerHTML) {
-          return innerHTML
-        }
-      },
-      {
-        filter: ['input', 'form', 'iframe', 'canvas', 'embed', 'select', 'rt', 'wbr'],
-        replacement (innerHTML) {
-          return ''
-        }
-      },
-      {
-        filter: ['pre'],
-        replacement (innerHTML) {
-          return '```\n' + stripTags(innerHTML) + '\n```\n'
-        }
-      }
-    ]
-  })
+const turndownService = new TurndownService()
+turndownService.addRule('block', {
+  filter: ['div', 'p', 'dt', 'dd', 'summary'],
+  replacement(innerHTML) {
+    return '\n' + innerHTML + '\n'
+  }
+})
+turndownService.addRule('inline-block', {
+  filter: [
+    'font',
+    'span',
+    'article',
+    'section',
+    'nav',
+    'button',
+    'main',
+    'footer',
+    'header',
+    'aside',
+    'details',
+    'u',
+    'samp',
+    'var',
+    'kbd',
+    'legend',
+    'mark',
+    'output',
+    'small',
+    'sub',
+    'sup'
+  ],
+  replacement(innerHTML) {
+    return innerHTML
+  }
+})
+turndownService.addRule('ignore', {
+  filter: ['input', 'form', 'iframe', 'canvas', 'embed', 'select', 'rt', 'wbr'],
+  replacement(_innerHTML) {
+    return ''
+  }
+})
+turndownService.addRule('code-block', {
+  filter: ['pre'],
+  replacement(innerHTML) {
+    return '```\n' + stripTags(innerHTML) + '\n```\n'
+  }
+})
+turndownService.use(gfm)
+
+export default function HTML2Markdown(html: string): string {
+  const md = turndownService.turndown(html)
   return stripHTMLTagsFromMarkdown(md)
 }
 
-function stripHTMLTagsFromMarkdown (value: string): string {
+function stripHTMLTagsFromMarkdown(value: string): string {
   const remark = require('unified')()
     .use(require('remark-parse'))
     .use(require('remark-stringify'), {
@@ -44,9 +66,12 @@ function stripHTMLTagsFromMarkdown (value: string): string {
       fences: true
     })
     .use(require('remark-strip-html'))
-  return remark().processSync(value).toString().trimRight()
+  return remark()
+    .processSync(value)
+    .toString()
+    .trimRight()
 }
 
-function stripTags (v: string): string {
+function stripTags(v: string): string {
   return v.replace(/<(?:.|\n)*?>/gm, '')
 }
--- test/index.js
+++ test/index.js
@@ -36,14 +36,17 @@ test('convert github code block', t => {
 });</pre>`
   const md = html2markdown(html)
   t.is(typeof md, 'string')
-  t.is(md, `\`\`\`
-var gpio = require("pi-gpio●");
+  t.is(
+    md,
+    `\`\`\`
+var gpio \\= require("pi-gpio●");
 gpio.open(16, "output", function(err) {    // Open pin 16 for output
   gpio.write(16, 1, function() {    // Set pin 16 high (1)
     gpio.close(16);            // Close pin 16
   });
 });
-\`\`\``)
+\`\`\``
+  )
 })
 
 test('convert list', t => {
