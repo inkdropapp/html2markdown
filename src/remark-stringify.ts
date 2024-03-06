31947dc180871f92f58f1267c856ed7230ad84e4
Takuya Matsuyama <hi@craftz.dog>
2024-03-06 22:37:01 +0900

    fix(type): clean up ts errors

--- src/remark-stringify.ts
+++ src/remark-stringify.ts
@@ -1,22 +1,15 @@
-import { Plugin, Compiler } from 'unified'
+import type { Plugin, Compiler } from 'unified'
 import { Options as ToMarkdownOptions } from 'mdast-util-to-markdown'
-import { Root } from 'mdast'
+import type { Root } from 'mdast'
 import { toMarkdown } from 'mdast-util-to-markdown'
-
-type Processor = import('unified').Processor<
-  undefined,
-  undefined,
-  undefined,
-  Root,
-  string
->
 
 export type Options = Omit<ToMarkdownOptions, 'extensions'>
 
-export function remarkStringify(
-  this: Processor,
-  options: Plugin<[Options?] | void[], Root, string>
-) {
+export const remarkStringify: Plugin<
+  [(Readonly<Options> | null | undefined)?],
+  Root,
+  string
+> = function (options: Readonly<Options> | null = {}) {
   const compiler: Compiler<Root, string> = tree => {
     // Assume options.
     const settings = this.data('settings') as Options
@@ -27,6 +20,8 @@ export function remarkStringify(
         // Note: this option is not in the readme.
         // The goal is for it to be set by plugins on `data` instead of being
         // passed by users.
+        // https://github.com/remarkjs/remark/blob/618a9ad1d44aa106bd2a8c61ebf8589cfe97fa16/packages/remark-stringify/lib/index.js#L32
+        //
         // @ts-ignore
         extensions: (this.data('toMarkdownExtensions') ||
           []) as ToMarkdownOptions['extensions']
@@ -34,5 +29,5 @@ export function remarkStringify(
     )
   }
 
-  this.compiler = compiler
+  this.compiler = compiler as any
 }
