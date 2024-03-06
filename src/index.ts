e5a6d7eec7ff6c81ff34abaf6760fb90151790fc
Takuya Matsuyama <hi@craftz.dog>
2024-03-06 15:33:33 +0900

    bump up to Unified 11 (#14)
    * 3.2.1
    
    * feat(dep): bump up unified to 11
    
    * fix(eslint): clean up

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { hardBreak } from './hard-break'
 import { code as toMdastCodeBlock } from './to-mdast-code-block'
 import { comment as toMdastComment } from './to-mdast-comment'
 
-import { unified } from 'unified'
+import { Processor, unified } from 'unified'
 import rehypeParse from 'rehype-parse'
 import rehype2remark from 'rehype-remark'
 import { rehypeInsertBaseURI } from './rehype-insert-base-uri'
@@ -13,6 +13,7 @@ import {
 import { squeezeLinks } from 'remark-squeeze-links'
 import { gfmToMarkdown } from 'mdast-util-gfm'
 import { Options as ToMdastOptions } from 'hast-util-to-mdast'
+import type { Root as HastRoot } from 'hast'
 
 export type Options = {
   toMdast?: ToMdastOptions
@@ -27,34 +28,38 @@ function getConverter(opts?: Options) {
     baseURI = null
   } = opts || {}
 
-  let remark = unified()
+  const remarkInit: Processor<HastRoot> = unified()
+    // @ts-ignore
     .data('toMarkdownExtensions', [gfmToMarkdown()])
     .use(rehypeParse)
-  if (baseURI) remark = remark.use(rehypeInsertBaseURI, baseURI || '/')
+  const remark = baseURI
+    ? remarkInit.use(rehypeInsertBaseURI, baseURI || '/')
+    : remarkInit
 
-  return (
-    remark
-      .use(rehype2remark, {
-        handlers: {
-          pre: toMdastCodeBlock,
-          comment: toMdastComment,
-          ...(toMdastOptions.handlers || {})
-        },
-        ...toMdastOptions
-      })
-      .use(squeezeLinks)
-      // @ts-ignore
-      .use(remarkStringify, {
-        listItemIndent: 'one',
-        bullet: '-',
-        fences: true,
-        handlers: {
-          break: hardBreak,
-          ...(stringifyOptions.handlers || {})
-        },
-        ...stringifyOptions
-      })
-  )
+  const remarkParser = remark
+    // @ts-ignore
+    .use(rehype2remark, {
+      handlers: {
+        pre: toMdastCodeBlock,
+        ...(toMdastOptions.handlers || {})
+      },
+      nodeHandlers: {
+        comment: toMdastComment,
+        ...(toMdastOptions.nodeHandlers || {})
+      },
+      ...toMdastOptions
+    })
+    .use(squeezeLinks)
+  return remarkParser.use(remarkStringify, {
+    listItemIndent: 'one',
+    bullet: '-',
+    fences: true,
+    handlers: {
+      break: hardBreak,
+      ...(stringifyOptions.handlers || {})
+    },
+    ...stringifyOptions
+  })
 }
 
 export function html2Markdown(html: string, opts?: Options): string {
@@ -63,4 +68,9 @@ export function html2Markdown(html: string, opts?: Options): string {
     .processSync(html)
     .toString() // unescape task list checkbox
     .replace(/\\\[(x| )\]/g, '[$1]')
+}
+
+export function parseMarkdown(html: string): HastRoot {
+  const c = getConverter()
+  return c.parse(html)
 }
